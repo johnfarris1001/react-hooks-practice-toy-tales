005bc03597f17022a17ffd790d597d886d9ff513
John <johnfarris217@gmail.com>
2023-04-03 14:00:56 -0500

    deliverables finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,49 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
+
+  const API = 'http://localhost:3001/toys'
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(data => setToys(data))
+  }, [])
+
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  function addNewToy(toy) {
+    setToys([...toys, toy])
+  }
+
+  function deleteItem(deletedItem) {
+    const updatedToys = toys.filter(item => item.id !== deletedItem.id)
+    setToys(updatedToys)
+  }
+
+  function addLike(updatedToy) {
+    const updatedToys = toys.map(toy => {
+      if (updatedToy.id === toy.id) {
+        return updatedToy
+      } else {
+        return toy
+      }
+    })
+    setToys(updatedToys)
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm api={API} toys={toys} addNewToy={addNewToy} /> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} api={API} deleteItem={deleteItem} addLike={addLike} />
     </>
   );
 }
--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ api, addNewToy }) {
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+
+  function enterName(e) {
+    setName(e.target.value)
+  }
+
+  function enterImage(e) {
+    setImage(e.target.value)
+  }
+
+  function addToy(e) {
+    e.preventDefault()
+    const newToy = {
+      name: name,
+      image: image,
+      likes: 0
+    }
+    fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+      .then(r => r.json())
+      .then(data => addNewToy(data))
+    setName('')
+    setImage('')
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={addToy}>
         <h3>Create a toy!</h3>
         <input
+          onChange={enterName}
+          value={name}
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
@@ -13,6 +46,8 @@ function ToyForm() {
         />
         <br />
         <input
+          onChange={enterImage}
+          value={image}
           type="text"
           name="image"
           placeholder="Enter a toy's image URL..."
